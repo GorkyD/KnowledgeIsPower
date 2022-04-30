8baae1f216395c754fc1e7d04e181e8e0ee4b7ec
Danila Harkavy <d.garkavyy@mail.ru>
2022-04-30 21:58:21 +0300

    Base

---
+++ Assets/CodeBase/Infrastructure/Game.cs
@@ -0,0 +1,16 @@
+using CodeBase.Services.Input;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+  public class Game
+  {
+    public static IInputService InputService;
+    public GameStateMachine StateMachine;
+
+    public Game()
+    { 
+        StateMachine = new GameStateMachine();
+    }
+  }
+}
